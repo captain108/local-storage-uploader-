4229dd458dac020fd3035977625a24b9fc7c195f
Captain <papajiop11@gmail.com>
2026-07-03 20:30:49 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///users.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Google OAuth
+    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
+    GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+
+    # Upload limits
+    MAX_CONTENT_LENGTH = 1024 * 1024 * 1024  # 1 GB per file
+    MAX_TOTAL_STORAGE_PER_USER = 5 * 1024 * 1024 * 1024  # 5 GB
